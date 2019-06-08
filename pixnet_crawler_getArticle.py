67a99c30a56d0dfc97cae127a93b018a4c98f164
yunwen <yunwenchenn@gmail.com>
2019-06-08 18:27:37 +0800

    revise crawler commit

--- pixnet_crawler_getArticle.py
+++ pixnet_crawler_getArticle.py
@@ -3,35 +3,62 @@
 
 # ### pixnet get Article
 
-# In[3]:
+# In[2]:
 
 
 import requests
 from lxml import etree
 import pandas as pd
 from bs4 import BeautifulSoup
+
+
+# In[3]:
+
+
 import time
+
+
+# In[4]:
+
+
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
 
-# In[33]:
+# In[6]:
+
+
 df = pd.read_csv("Pinex_bytags.csv")
 df = df.iloc[:,1:]
 df
 
 
-# In[42]:
+# In[10]:
+
+
 article_urls = df["article_url_list"].tolist()
+article_urls = article_urls[0:1]
 article_urls
 
 
+# In[43]:
+
+
+len(article_urls)
+
+
 # In[44]:
+
+
 article_urls = set(article_urls)
+len(article_urls)
 
 
+# ### 6/8 13:35pm 
 
 # In[48]:
+
+
 def get_Article(urls):
     for i in urls:
         session = requests.Session()
@@ -53,8 +80,15 @@ def get_Article(urls):
         return article_title_2nd, article_content
 
 
+# In[51]:
+
+
+len(article_title_2nd_list)
+
 
 # In[49]:
+
+
 article_title_2nd_list = []
 article_content_list = []
 article_url_list_2nd = []
@@ -67,9 +101,15 @@ for i in article_urls:
         article_title_2nd_list.extend([article_title_2nd])
         article_content_list.extend([article_content])
         article_url_list_2nd.extend([i])
+#     article_author_list.extend(article_author)
+#     article_title_list.extend(article_title)
+#     article_url_list.extend(article_url)
+#     article_tags_list.extend(article_tags)
 
 
 # In[31]:
+
+
 df2 = pd.DataFrame()
 df2["article_url_list"] = article_url_list_2nd
 # df2["article_title"] = article_title_2nd_list
@@ -78,10 +118,14 @@ df2
 
 
 # In[32]:
+
+
 result = pd.merge(df, df2, on='article_url_list')
 result
 
 
 # In[28]:
-result.to_csv(r'articles.csv', mode='a', encoding='utf_8_sig')
+
+
+df.to_csv(r'text02.csv', mode='a', encoding='utf_8_sig')   #存成csv檔
 
